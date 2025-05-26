1720793a8ff3b73df4cea4e45034a9f3d7816918
Fede Antunovic <federxv@gmail.com>
2025-05-26 14:38:24 -0400

    Fix asset paths for custom domain

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/landing_maia' : '',
+  basePath: '',
   trailingSlash: true,
 };
 
